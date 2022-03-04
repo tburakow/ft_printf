8d0775cb128aa47f41ff3d054f54abc158a63fcb
Tuomas Burakowski <tburakow@Tuomas-MacBook-Air.local>
2022-03-04 20:43:22 +0200

    Fixed length modifiers, most likely.

--- formats_one.c
+++ formats_one.c
@@ -6,7 +6,7 @@
 /*   By: tburakow <tburakow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 11:44:05 by tburakow          #+#    #+#             */
-/*   Updated: 2022/03/04 15:30:44 by tburakow         ###   ########.fr       */
+/*   Updated: 2022/03/04 20:42:41 by tburakow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	signed_int(va_list *arg, t_flags **flags)
 	char		*string_form;
 
 	if ((*flags)->l == 2)
-		nbr = (long int)va_arg(*arg, long);
-	else if ((*flags)->l == 1)
 		nbr = (long long int)va_arg(*arg, long long);
+	else if ((*flags)->l == 1)
+		nbr = (long int)va_arg(*arg, long);
 	else if ((*flags)->h == 2)
 		nbr = (signed char)va_arg(*arg, int);
 	else if ((*flags)->h == 1)
@@ -38,43 +38,79 @@ void	signed_int(va_list *arg, t_flags **flags)
 
 void	unsigned_octal(va_list *arg, t_flags **flags)
 {
-	unsigned	integer;
+	long long	nbr;
 	char		*string_form;
 	
-	integer = va_arg(*arg, unsigned);
-	string_form = ft_itoa(octal_conversion(integer));
+	if ((*flags)->l == 2)
+		nbr = (unsigned long long int)va_arg(*arg, long long);
+	else if ((*flags)->l == 1)
+		nbr = (unsigned long int)va_arg(*arg, long);
+	else if ((*flags)->h == 2)
+		nbr = (unsigned char)va_arg(*arg, int);
+	else if ((*flags)->h == 1)
+		nbr = (unsigned short int)va_arg(*arg, int);
+	else
+		nbr = va_arg(*arg, unsigned int);
+	string_form = ft_itoa(octal_conversion(nbr));
 	string_form = apply_flags(string_form, flags);
 	print_out(string_form);
 }
 
 void	unsigned_dec(va_list *arg, t_flags **flags)
 {
-	unsigned	integer;
+	long long	nbr;
 	char		*string_form;
 	
-	integer = va_arg(*arg, unsigned);
+	if ((*flags)->l == 2)
+		nbr = (unsigned long long int)va_arg(*arg, long long);
+	else if ((*flags)->l == 1)
+		nbr = (unsigned long int)va_arg(*arg, long);
+	else if ((*flags)->h == 2)
+		nbr = (unsigned char)va_arg(*arg, int);
+	else if ((*flags)->h == 1)
+		nbr = (unsigned short int)va_arg(*arg, int);
+	else
+		nbr = va_arg(*arg, unsigned int);
 	string_form = apply_flags(ft_itoa(integer), flags);
 	print_out(string_form);
 }
 
 void	unsigned_hex(va_list *arg, t_flags **flags)
 {
-	unsigned	integer;
+	long long	nbr;
 	char		*hexadec;
-	
-	integer = va_arg(*arg, unsigned);
-	hexadec = hex_conversion(integer);
+
+	if ((*flags)->l == 2)
+		nbr = (unsigned long long int)va_arg(*arg, unsigned long long);
+	else if ((*flags)->l == 1)
+		nbr = (unsigned long int)va_arg(*arg, unsigned long);
+	else if ((*flags)->h == 2)
+		nbr = (unsigned char)va_arg(*arg, unsigned int);
+	else if ((*flags)->h == 1)
+		nbr = (unsigned short int)va_arg(*arg, unsigned int);
+	else	
+		nbr = va_arg(*arg, unsigned int);
+	hexadec = hex_conversion(nbr);
 	hexadec = apply_flags(hexadec, flags);
 	print_out(hexadec);
 }
 
 void	unsigned_hex_cap(va_list *arg, t_flags **flags)
 {
-	unsigned	integer;
+	unsigned	nbr;
 	char		*hexadec;
 	
-	integer = va_arg(*arg, unsigned);
-	hexadec = hex_cap_conversion(integer);
+	if ((*flags)->l == 2)
+		nbr = (unsigned long long int)va_arg(*arg, unsigned long long);
+	else if ((*flags)->l == 1)
+		nbr = (unsigned long int)va_arg(*arg, unsigned long);
+	else if ((*flags)->h == 2)
+		nbr = (unsigned char)va_arg(*arg, unsigned int);
+	else if ((*flags)->h == 1)
+		nbr = (unsigned short int)va_arg(*arg, unsigned int);
+	else	
+		nbr = va_arg(*arg, unsigned int);
+	hexadec = hex_cap_conversion(nbr);
 	hexadec = apply_flags(hexadec, flags);
 	print_out(hexadec);
 }
